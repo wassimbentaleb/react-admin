4edd308b8265ac25472521221dbec7ef32dbfe6c
Wassim Ben Taleb <wassimwbt@gmail.com>
2023-12-28 13:05:24 +0100

    FAQ Page with MUI Accordion

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Contacts from "./scenes/contacts"
 import Form from "./scenes/form"
 //import Line from "./scenes/line"
 //import Pie from "./scenes/pie"
-//import FAQ from "./scenes/faq"
+import FAQ from "./scenes/faq"
 //import Geagraphy from "./scenes/ggeagraphy"
 import Calendar from "./scenes/calendar"
 
@@ -33,13 +33,14 @@ function App() {
             <Route path="/" element={<Dashboard/>}/>
             <Route path="/team" element={<Team/>}/>
            <Route path="/contacts" element={<Contacts/>}/>
-             <Route path="/invoices" element={<Invoices/>}/>
+            <Route path="/invoices" element={<Invoices/>}/>
             <Route path="/form" element={<Form/>}/>
             <Route path="/calendar" element={<Calendar/>}/>
+            <Route path="/faq" element={<FAQ/>}/>
            {/* <Route path="/bar" element={<Bar/>}/>
             <Route path="/pie" element={<Pie/>}/>
             <Route path="/line" element={<Line/>}/>
-            <Route path="/faq" element={<FAQ/>}/>
+            
             <Route path="/geography" element={<Geagraphy/>}/>*/}
            
           </Routes>
